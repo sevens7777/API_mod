6c909654b8b41678792b42dd115c0cd1aa217a4e
sevens7777 <65147810+sevens7777@users.noreply.github.com>
2025-03-21 18:58:35 +0800

    Update main.go

--- relay/adaptor/ali/main.go
+++ relay/adaptor/ali/main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/songquanpeng/one-api/common/helper"
 	"github.com/songquanpeng/one-api/common/logger"
 	"github.com/songquanpeng/one-api/relay/adaptor/openai"
-	"github.com/songquanpeng/one-api/relay/model"
+	"github.com/sevens7777/one-api-mod/relay/model"
 )
 
 // https://help.aliyun.com/document_detail/613695.html?spm=a2c4g.2399480.0.0.1adb778fAdzP9w#341800c0f8w0r
